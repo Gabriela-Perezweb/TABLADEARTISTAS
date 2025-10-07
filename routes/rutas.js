bf0082760ef6ce3ed98970049b971cf398501a04
GabrielaPerez <gabrielaps.ti24@utsjr.edu.mx>
2025-10-07 07:34:36 -0600

    mi primer commit

---
+++ routes/rutas.js
@@ -0,0 +1,32 @@
+import { Router } from "express"
+
+const router = Router()
+
+// Ejemplo de datos
+var artistas = ["van gogh", "beethoven", "mozart", "da vinci"]
+
+// Página principal
+router.get("/", (req, res) => {
+  res.render("home", { artistas })
+})
+
+// Página info
+router.get("/info/:c/:texto", (req, res) => {
+  var c = req.params.c
+  var texto = req.params.texto
+  console.log(c)
+  res.render("info", { c, texto })
+})
+
+
+router.get("/contacto", (req, res) => {
+  res.render("contact")
+})
+
+
+router.post("/enviar-contacto", (req, res) => {
+  const { nombre, edad } = req.body
+  res.send(Gracias ${nombre}, hemos recibido tu información. Tu edad es ${edad}.)
+})
+
+export default router
